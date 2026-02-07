51dcf009311933d842f3d89a1b071ac97f9a0114
Kavineash J B <kavineashcsc@gmail.com>
2026-02-07 19:24:17 +0530

    Updated 10_String_Handling.sql

--- Learning/10_String_Handling.sql
+++ Learning/10_String_Handling.sql
@@ -16,6 +16,10 @@ SELECT * from customers;
 -- reverse(str)
 -- format(decimal,precision) => returns formatted to specified no.of decimal places as comma separated(especially for price)
 -- ** we can also use function inside another function like length(upper(concat(str1,str2,..))) as str_len
+-- substring_index(str, deli, count) => split string
+-- Eg:
+-- SELECT SUBSTRING_INDEX(SUBSTRING_INDEX('A,B,C,D', ',', 3),',',-1);   -- OP: C
+
 
 -- select substring of name with 5 chars + '...' as suffix
 select name, 
