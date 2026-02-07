0597f7d208a851ffa97cedf0290f0f596d515896
Kavineash J B <kavineashcsc@gmail.com>
2026-02-07 18:32:49 +0530

    Update Alter.sql

--- Learning/03_Alter.sql
+++ Learning/03_Alter.sql
@@ -7,13 +7,20 @@ select * from tb;
 -- columns
 -- add column
 alter table tb add column city varchar(25);
--- rename column
+-- rename table name
 alter table tb rename to renamed_tb;
 desc renamed_tb;
 
 -- rename renamed_tb to tb
 alter table renamed_tb rename to tb;
+
+-- rename col name
+alter table tb rename column city to cities;
 desc tb;
+
+alter table tb rename column cities to city;
+
+
 select * from tb;
 -- drop column
 alter table tb drop column city;
